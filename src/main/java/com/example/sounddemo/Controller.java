1c4a2f34f81d94a492624d998fa52559d0518fc1
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-02-15 09:12:33 -0500

    One working combo box

--- src/main/java/com/example/sounddemo/Controller.java
+++ src/main/java/com/example/sounddemo/Controller.java
@@ -1,8 +1,11 @@
 package com.example.sounddemo;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -15,13 +18,27 @@ public class Controller implements Initializable {
     public Button btn4;
     public Button btn3;
     public Button btn2;
-    public ComboBox combo1;
-    public ComboBox combo2;
-    public VBox vBox;
 
+    public ComboBox<String> combo2;
+    public VBox vBox;
+    public AnchorPane rootPane;
+
+    private ObservableList<String> choiceListArray = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        String[] fruits = {"Option 1", "Option 2", "option 3"};
 
+
+        String[] week_days =
+                {"Monday", "Tuesday", "Wednesday",
+                        "Thursday", "Friday"};
+
+        // Create a combo box
+        ComboBox combo_box = new ComboBox(FXCollections.observableArrayList(week_days));
+
+
+        rootPane.getChildren().add(combo_box);
     }
+
 }
