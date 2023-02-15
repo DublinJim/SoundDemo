8588ed215f377bb065ad60b52f78b05120cc8751
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-02-15 15:43:24 -0500

    both combo boxes  some function

--- src/main/java/com/example/sounddemo/Controller.java
+++ src/main/java/com/example/sounddemo/Controller.java
@@ -7,8 +7,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.util.EventListener;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -19,28 +21,38 @@ public class Controller implements Initializable {
     public Button btn3;
     public Button btn2;
 
-    public ComboBox<String> combo2;
+    public ComboBox<String> comboBox2;
     public VBox vBox;
     public AnchorPane rootPane;
+    public Text txtOutput;
 
     private ObservableList<String> choiceListArray = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         String[] fruits = {"Option 1", "Option 2", "Option 3"};
-
-
-        String[] week_days =
-                {"Select Day","Monday", "Tuesday", "Wednesday",
-                        "Thursday", "Friday"};
+        String[] week_days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
 
         // Create a combo box
-        ComboBox combo_box = new ComboBox(FXCollections.observableArrayList(week_days));
-combo_box.getSelectionModel().select(week_days[0]);
-        rootPane.getChildren().add(combo_box);
-        combo2 =new ComboBox<>(FXCollections.observableArrayList(fruits));
-        vBox.getChildren().add(combo2);
-        combo2.getSelectionModel().select(fruits[0]);
+        ComboBox comboBox1 = new ComboBox(FXCollections.observableArrayList(week_days));
+        comboBox2 = new ComboBox<>(FXCollections.observableArrayList(fruits));
+
+        comboBox1.setPromptText("Days");
+        comboBox2.setPromptText("Options");
+
+        vBox.getChildren().add(comboBox2);
+        vBox.getChildren().add(comboBox1);
+
+btn1.setOnAction(e->{
+    getOutput();
+});
+
     }
+
+    public void getOutput()
+    {
+        txtOutput.setText(comboBox2.getValue());
+    }
+
 
 }
