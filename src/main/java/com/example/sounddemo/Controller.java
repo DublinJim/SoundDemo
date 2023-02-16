3d0365701cbcd983f2dd08fbdf2466fde09a7529
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-02-16 11:24:57 -0500

    adds inline label modifications

--- src/main/java/com/example/sounddemo/Controller.java
+++ src/main/java/com/example/sounddemo/Controller.java
@@ -4,8 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -26,26 +28,23 @@ public class Controller implements Initializable {
 
     public ComboBox<String> comboBox1;
     public Text txtOutput2;
-    public String[] fruits;
+    public String[] options;
     public String[] week_days;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         //create the arrays for choices
-        fruits = new String[]{"Option 1", "Option 2", "Option 3"};
+        options = new String[]{"Option 1", "Option 2", "Option 3"};
         week_days = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
 
         // Create a combo box
         comboBox1 = new ComboBox<>(FXCollections.observableArrayList(week_days));
-        comboBox2 = new ComboBox<>(FXCollections.observableArrayList(fruits));
+        comboBox2 = new ComboBox<>(FXCollections.observableArrayList(options));
 
         //set the default text
         comboBox1.setPromptText("Days");
         comboBox2.setPromptText("Options");
-
-        //add to the Vbox
-        vBox.getChildren().add(comboBox2);
-        vBox.getChildren().add(comboBox1);
 
         //blank the second text block
         txtOutput2.setText(null);
@@ -59,8 +58,15 @@ public class Controller implements Initializable {
         btn2.setPrefWidth(100);
         btn2.setOnAction(e -> getOutputCombo1());
 
-
-
+        //create a label
+        Label lbl1 = new Label("Submit Choices");
+        lbl1.setLayoutY(6);
+        lbl1.setLayoutX(80);
+        lbl1.setFont(new Font("Franklin Gothic", 20));
+        //add to the Vbox
+        vBox.getChildren().add(comboBox2);
+        vBox.getChildren().add(comboBox1);
+        rootPane.getChildren().add(lbl1);
 
     }
 
